81275f9d539abc2e1a3e58ae1c01cfe99738969e
Navinoxx <lorenzo0100@hotmail.com>
2023-08-31 22:53:11 -0300

    update for deploy

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' in os.environ
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
@@ -168,7 +168,7 @@ AUTH_USER_MODEL = 'users.User'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'https://roaring-zabaione-66e8cc.netlify.app',
 ]
 
 REST_FRAMEWORK = {
