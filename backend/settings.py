20bd08bd3b66d1a6a29e75635bde8e8d2c88577b
Navinoxx <lorenzo0100@hotmail.com>
2023-09-12 23:27:11 -0300

    update settings

--- backend/settings.py
+++ backend/settings.py
@@ -107,13 +107,14 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if not DEBUG:
-    DATABASES = {'default': dj_database_url.config(os.environ.get('DATABASE_URL'))}
+    DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }}
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
