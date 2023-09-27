b94680a3bd6e29f1a1423327aa3aadbaf0c18dc8
Navinoxx <lorenzo0100@hotmail.com>
2023-09-27 20:08:27 -0300

    redis config

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from . import consumers
+from .consumers import PersonalChatConsumer
 
 websocket_urlpatterns = [
-    path("ws/<str:username>/<str:my_username>/", consumers.PersonalChatConsumer.as_asgi()),
+    path("ws/<str:username>/<str:my_username>/", PersonalChatConsumer.as_asgi()),
 ]
