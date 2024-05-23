42228d4d1e9d8e3f61c3d4bcf2f44101ab1e0536
Navinoxx <lorenzo0100@hotmail.com>
2024-05-22 23:19:05 -0300

    redis config

--- backend/settings.py
+++ backend/settings.py
@@ -96,7 +96,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.getenv('REDIS_URL')],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     }
 }
