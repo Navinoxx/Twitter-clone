8dde09b35fb3796165eb12ce4e4f8e5dfe749b28
Navinoxx <lorenzo0100@hotmail.com>
2024-05-23 21:10:26 -0300

    prepare to deploy

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+export DJANGO_SETTINGS_MODULE=backend.settings
 
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
